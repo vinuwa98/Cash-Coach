86302944856484d78ad1ad97749156929ffeb4ff
vinuwa98 <116092558+vinuwa98@users.noreply.github.com>
2025-01-04 22:38:10 +0530

    Add Override method

--- app/src/main/java/com/example/myapplication/NextIncome.java
+++ app/src/main/java/com/example/myapplication/NextIncome.java
@@ -10,7 +10,30 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+public class NextIncome extends AppCompatActivity {
+    Button go;
+    EditText e1,e2,e3;
+    String base,LTA,HRA,SA;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_income);
+        e1=(EditText)findViewById(R.id.edit1);
+        e2=(EditText)findViewById(R.id.edit2);
+        e3=(EditText)findViewById(R.id.edit3);
+        Intent i=getIntent();
+        base=i.getStringExtra("baseincome");
+        LTA=i.getStringExtra("LTA");
+        HRA=i.getStringExtra("HRA");
+        SA=i.getStringExtra("SA");
 
+        go=(Button)findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
     public void finish(){
