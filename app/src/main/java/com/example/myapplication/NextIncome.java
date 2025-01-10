b6e7839ed3274eb747c080f054963343c87dcd3d
vinuwa98 <116092558+vinuwa98@users.noreply.github.com>
2025-01-10 23:43:07 +0530

    Update NextIncome.java

--- app/src/main/java/com/example/myapplication/NextIncome.java
+++ app/src/main/java/com/example/myapplication/NextIncome.java
@@ -36,6 +36,19 @@ public class NextIncome extends AppCompatActivity {
         });
 
     }
+    public void finish(){
+        double A=Double.parseDouble(e1.getText().toString());
+        double B=Double.parseDouble(e2.getText().toString());
+        double C=Double.parseDouble(e3.getText().toString());
+        Intent i=new Intent(NextIncome.this,Finalinc.class);
+        i.putExtra("80A",e1.getText().toString());
+        i.putExtra("base",base);
+        i.putExtra("hra",HRA);
+        i.putExtra("lta",LTA);
+        i.putExtra("sa",SA);
+        i.putExtra("80B",e2.getText().toString());
+        i.putExtra("80C",e3.getText().toString());
+        startActivity(i);
 
     }
 }
