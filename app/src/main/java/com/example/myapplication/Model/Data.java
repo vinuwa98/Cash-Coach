f5ff23baaf630393d58eb5313ab96ca732ba34fa
vinuwa98 <116092558+vinuwa98@users.noreply.github.com>
2025-01-16 22:24:23 +0530

    xxx

--- app/src/main/java/com/example/myapplication/Model/Data.java
+++ app/src/main/java/com/example/myapplication/Model/Data.java
@@ -26,36 +26,7 @@ public class Data {
 
 
 
-    public int getAmount(){return amount;}
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
+    
     public String getDate() {
         return date;
     }
